da243ea01921d733ab0e399c57346252ebccc0c2
Fritz Reese <fritzoreese@gmail.com>
2021-12-13 16:23:31 -0500

    Simple adjacency graph for Day 12

--- src/d12/mod.rs
+++ src/d12/mod.rs
@@ -1,15 +1,64 @@
 use std::io::BufRead;
-use crate::{cli, Day, PartResult, util, util::vec2d::Vec2d};
+use crate::{cli, Day, PartResult};
 use std::error::Error;
+use std::collections::HashMap;
+use std::fmt::{Display, Formatter};
 
 pub struct Day12;
+
+#[derive(Debug)]
+struct Graph {
+    adj: HashMap<String, Vec<String>>,
+}
+
+impl Graph {
+    fn from(input: &mut dyn BufRead) -> Result<Graph, Box<dyn Error>> {
+        let mut adj = HashMap::<String, Vec<String>>::new();
+        for line in input.lines() {
+            let line = line?;
+            let mut items = line.split("-");
+            let node1 = items.next().ok_or("missing left node")?;
+            let node2 = items.next().ok_or("missing right node")?;
+            adj.entry(node1.to_string())
+                .or_insert_with(|| Vec::<String>::new())
+                .push(node2.to_string());
+            adj.entry(node2.into())
+                .or_insert_with(|| Vec::new())
+                .push(node1.into())
+        }
+        Ok(Graph { adj })
+    }
+
+    fn count_paths(&self, _from: String, _to: String) -> usize {
+        0 // TODO
+    }
+}
+
+impl Display for Graph {
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result<(), std::fmt::Error>
+    {
+        for (key, values) in self.adj.iter() {
+            write!(f, "{:>5}: |{}|", key, values.len())?;
+            for value in values {
+                write!(f, " {:>5}", value)?;
+            }
+            write!(f, "\n")?;
+        }
+        Ok(())
+    }
+}
 
 impl Day for Day12 {
     fn mod_path(&self) -> &str { file!() }
     fn run(&self, input: &mut dyn BufRead, opts: &cli::Cli)
         -> Result<(PartResult, PartResult), Box<dyn Error>>
     {
-        Ok((PartResult::new(), PartResult::new()))
+        let g = Graph::from(input)?;
+        if opts.verbose {
+            println!("{}", g);
+        }
+        Ok((PartResult::from(|| g.count_paths("start".into(), "end".into())),
+            PartResult::new()))
     }
 }
 
