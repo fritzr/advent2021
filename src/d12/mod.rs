f122582f4230f173a4eddf4bd1cbddeda529a803
Fritz Reese <fritzoreese@gmail.com>
2021-12-14 00:04:27 -0500

    WIP Day 12

--- src/d12/mod.rs
+++ src/d12/mod.rs
@@ -11,6 +11,63 @@ struct Graph {
     adj: HashMap<String, Vec<String>>,
 }
 
+// Concatenation of (string) nodes forming a unique subpath.
+//
+// We abuse the fact that intermediate nodes always have length 2.
+// This means the input shouldn't contain "rt" or "nd" as separate nodes,
+// as that will be how "start" and "end" are stored.
+type Subpath = String;
+
+struct PathCounter<'a> {
+    g: &'a Graph,
+    start: String,
+    end: String,
+    counts: HashMap<Subpath, usize>,
+}
+
+fn is_lower(s: &String) -> bool {
+    s.find(char::is_uppercase) == None
+}
+
+fn is_upper(s: &String) -> bool {
+    s.find(char::is_lowercase) == None
+}
+
+impl<'a> PathCounter<'a> {
+    fn from(g: &'a Graph, start: String, end: String) -> Self {
+        // assert_eq!(is_lower(start), true);
+        Self { g, start, end, counts: HashMap::with_capacity(g.adj.len()) }
+    }
+
+    fn count_paths(&mut self, prefix: Subpath, node: String)
+        -> usize
+    {
+        if node == self.end {
+            1
+        } else {
+            let previous = if prefix.len() > 1 {
+                prefix[(prefix.len()-2)..prefix.len()]
+            } else {
+                self.start.to_string()
+            };
+            let prefix = prefix + &node[(node.len()-2)..node.len()];
+            counts.entry(*prefix).or_insert(|| self.g.adj[node].filter_map(|adj| {
+                // Don't visit the previous node, and don't visit lowercase nodes twice.
+                if adj != previous && (!is_lower(node) || subpath.find(node) == None) {
+                    Some(self.count_paths(*prefix, adj))
+                } else {
+                    None
+                }
+            }).sum())
+        }
+    }
+
+    fn count(&mut self) -> usize {
+        let prefix = String::with_capacity(2 * self.g.adj.len());
+        self.count_paths(prefix, self.start)
+    }
+}
+
 impl Graph {
     fn from(input: &mut dyn BufRead) -> Result<Graph, Box<dyn Error>> {
         let mut adj = HashMap::<String, Vec<String>>::new();
@@ -29,8 +86,8 @@ impl Graph {
         Ok(Graph { adj })
     }
 
-    fn count_paths(&self, _from: String, _to: String) -> usize {
-        0 // TODO
+    fn count_paths(&self, from: String, to: String) -> usize {
+        PathCounter::from(self, from, to).count()
     }
 }
 
